81155fa8150c1d4a364a8c97e84e0d053da63ea9
Nicole Tan <000431652@student.vcc.ca>
2020-11-12 22:47:26 -0800

    added modifyamenity controller

---
+++ controllers/ModifyAmenity-FEATURE-nta-52.js
@@ -0,0 +1,23 @@
+const { readdirSync } = require("fs")
+
+const getAmenity = function(req, res, next) {
+    res.setHeader('Content-type', 'application/json');
+    res.send(amenities)
+}
+
+// exports.addAmenity = function(req, res, next) {
+//     res.setHeader('Content-type', 'application/json');
+//     res.send(JSON.stringify())
+// }
+
+const updateAmenity = function(req, res, next) {
+    var updateAmenityType = req.body.amenityType;
+    res.setHeader('Content-type', 'application/json');
+    amenity = amenities.map((amenity) => {amenity.amenityType == updateAmenityType})
+    res.send(amenities)
+}
+
+module.exports = [
+    getAmenity,
+    updateAmenity
+]
