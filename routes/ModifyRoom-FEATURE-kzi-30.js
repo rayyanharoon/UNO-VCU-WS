4f23fcdb43831e0bfed47059fc14800a699511f6
Karina Zialcita <000428830@student.vcc.ca>
2020-12-15 09:36:54 -0800

    fixed route getRoom

--- routes/ModifyRoom-FEATURE-kzi-30.js
+++ routes/ModifyRoom-FEATURE-kzi-30.js
@@ -7,7 +7,7 @@ var route = express.Router();
 var controller = require('../controllers/ModifyRoom-FEATURE-kzi-30');
 
 route.post('/modifyroom-kzi-30', controller.createRoom);
-route.get('/modifyroom-kzi-30', controller.getRoom);
+// route.get('/modifyroom-kzi-30', controller.getRoom);
 route.get('/modifyroom-kzi-30', controller.getRooms);
 // route.put('/modifyroom-kzi-30', controller.modifyRoom);
 route.delete('/modifyroom-kzi-30', controller.deleteRoom);
