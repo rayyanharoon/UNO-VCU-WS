92d4090038fbd20e868339577d35b62c4d525b51
Karina Zialcita <000428830@student.vcc.ca>
2020-12-17 14:32:25 -0800

    minor db url change

--- models/Room-kzi-30.js
+++ models/Room-kzi-30.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const {usr, psw} = require('../config')
 
 const schema = mongoose.Schema({
     roomType: String,
@@ -8,8 +9,7 @@ const schema = mongoose.Schema({
     versionKey: false
 })
 
-
-var roomdb = mongoose.createConnection('mongodb://localhost/rooms', {useNewUrlParser: true, useUnifiedTopology: true });
+var roomdb = mongoose.createConnection(`mongodb+srv://${usr}:${psw}@atlaslab.e3ssv.mongodb.net/rooms?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true });
 const roomSchema = roomdb.model("room", schema);
 
 module.exports = roomSchema;
